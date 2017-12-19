0f69e49449ad34efda0eee83074aac70d042266d
dennis0302 <denniskim0302@gmail.com>
2017-12-18 22:59:07 -0500

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY ='yi83X0YVxyWaZ8H3fLlTi2uXV'
+CONSUMER_SECRET = '6t91w1teUs6iCOWyPRitEZAc36UhO44DGJFZXKwKMXReUfcdKV'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
