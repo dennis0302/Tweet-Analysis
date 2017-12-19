d0cecd425e134089691669698eea51c93b2c68e4
dennis0302 <denniskim0302@gmail.com>
2017-12-18 23:11:22 -0500

    Update twitter_utils.py

--- twitter_utils.py
+++ twitter_utils.py
@@ -7,19 +7,17 @@ consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
 
 def get_request_token():
     client = oauth2.Client(consumer)
-
     response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
     if response.status != 200:
         print("An error occurred getting the request token from Twitter!")
-
     return dict(urlparse.parse_qsl(content.decode('utf-8')))
 
 
 def get_oauth_verifier(request_token):
     print("Go to the following site in your browser:")
     print(get_oauth_verifier_url(request_token))
-
     return input("What is the PIN? ")
+
 
 def get_oauth_verifier_url(request_token):
     return "{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token'])
